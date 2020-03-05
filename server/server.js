9c558d869247078db8e3c5dd96cb171fc6afe4e9
Sagar Aryal <s3aryal@uwaterloo.ca>
2020-03-04 21:17:07 -0500

    init commit

--- server/server.js
+++ server/server.js
@@ -2,35 +2,62 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const organizations = {}
-let currId = 0
-
-
+const organizations = {
+	0: { 'Name': 'Nandu Business', 'Net Worth': '6 Billion', 'Business Type': 'Real Estate Dealer' },
+	1: { 'Name': 'Sailesh Business', 'Net Worth': '20 Billion', 'Business Type': 'Food Dealer' }
+}
+const event = {
+	0: {'event': 'Teej', 'Location': 'niggerLand', 'Date': '9/11', 'Price': '$69' },
+	1: {'event': 'Christmas', 'Location': 'CuckLand',  'Date': '4/20', 'Price': '$69'}
+}
 
 app.get('/', (req, res) => res.send('Hello United Nepali!'))
 
 app.get('/health-check', (req, res) => res.send('OK'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/nepali-check', (req, res) => res.send('Bhaat Khaiyo?'))
 
 app.get('/organizations', (req, res) => res.send(organizations))
 
-app.post('/organizations', function(req, res){
-	let id_val = currId;
-	currId = currId + 1;
-
-	organizations[id_val] = {'Name':req.body.name};
-	res.sendStatus(200);
-
+app.get('/organizations_id', function (req, res) {
+	var id = req.query.id;
+	let org = organizations[id]
+	res.send(org)
 })
 
+app.delete('/organization_id', function (req, res) {
+    var id = req.query.id;
+    let org = organizations[id]
+    delete organizations[id]
+    res.sendStatus(200)
+})
 
-// * [GET] [organizations/:id] Gets organization by ID
-// * [POST] [organizations/:id] Creates new organization by ID 
-// * [PUT] [organizations/:id] Updates new organization by ID 
-// * [DELETE] [organizations/:id] Delete organization by ID
+app.post('/organization_id', function(req, res) {
+	var id = req.query.id;
+	organizations[id] = req.query;
+	res.sendStatus(200);
+})
 
+app.put('/organization_id', function(req, res) {
+	var id = req.query.id;
+	var org = organizations[id];
 
+	for(var key in req.query){
+		if (key in org){
+			org[key] = req.query[key];
+		}
+	}
+	organizations[id] = org;
+
+	res.sendStatus(200);
+})
+
+app.get('/event', (req, res) => res.send(event))
+
+app.get('/event_details', function(req, res){
+	var id = req.query.id;
+	let lit = event[id]
+	res.send(lit)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${ port }!`))
