12d9049ca6ff559a7e6a38810ea4e74155314e3a
sonalranjit <sonal.ranjit3@gmail.com>
2020-03-30 00:23:04 +0000

    Deploying to gh-pages from  - c7e358679d58d01669a4722c679de36a581a4046 🚀

---
+++ precache-manifest.9a5394490760e85eeae800985218fc4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a66eadf1a2993ffa8704b8170dc2d3b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd01b8ea91d543730d75",
+    "url": "/static/css/main.d9c9e0fc.chunk.css"
+  },
+  {
+    "revision": "aec8acada171f264c4b4",
+    "url": "/static/js/2.56d0978d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.56d0978d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd01b8ea91d543730d75",
+    "url": "/static/js/main.f8a6bfe9.chunk.js"
+  },
+  {
+    "revision": "b86a97ce170b07d307a1",
+    "url": "/static/js/runtime-main.01edfc0a.js"
+  }
+]);
