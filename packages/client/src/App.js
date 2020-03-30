c7e358679d58d01669a4722c679de36a581a4046
Sonal Ranjit <sonal.ranjit3@gmail.com>
2020-03-29 20:20:45 -0400

    removed unused Link Component

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -5,7 +5,7 @@ import { Home } from "./components/Home";
 import { Mission } from "./components/Mission";
 import { DefaultPage } from "./components/DefaultPage";
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default function App() {
   return (
