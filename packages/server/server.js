31f8ae523d95735d5df1b2d1d081e5257f2c700c
Sonal Ranjit <sranjit@bluecatnetworks.com>
2020-05-02 13:05:42 -0400

    added eslint to the project

--- packages/server/server.js
+++ packages/server/server.js
@@ -1,36 +1,29 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+
+const app = express();
 
 const bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const organizations = {}
-let currId = 0
+const organizations = {};
+let currId = 0;
 
 
+app.get('/api', (req, res) => res.send('Hello United Nepali!'));
 
-app.get('/api', (req, res) => res.send('Hello United Nepali!'))
+app.get('/api/health-check', (req, res) => res.send('OK'));
 
-app.get('/api/health-check', (req, res) => res.send('OK'))
+app.post('/api/organizations', (req, res) => {
+  const idVal = currId;
+  currId += 1;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-app.get('/api/organizations', (req, res) => res.send(organizations))
-
-app.post('/api/organizations', function(req, res){
-	let id_val = currId;
-	currId = currId + 1;
-
-	organizations[id_val] = {'Name':req.body.name};
-	res.sendStatus(200);
-
-})
+  organizations[idVal] = { Name: req.body.name };
+  res.sendStatus(200);
+});
 
 
 // * [GET] [organizations/:id] Gets organization by ID
-// * [POST] [organizations/:id] Creates new organization by ID 
-// * [PUT] [organizations/:id] Updates new organization by ID 
+// * [POST] [organizations/:id] Creates new organization by ID
+// * [PUT] [organizations/:id] Updates new organization by ID
 // * [DELETE] [organizations/:id] Delete organization by ID
-
-
