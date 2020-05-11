db84186989ca483c9af5e8cf8f37d5b7a2a9dbd5
Binam Kayastha <binamkayastha@gmail.com>
2020-05-10 20:03:58 -0400

    Move docusaurus docs folder into website folder
    Update siteConfig.js with updated docs folder location.

--- packages/documentation/website/siteConfig.js
+++ packages/documentation/website/siteConfig.js
@@ -105,6 +105,7 @@ const siteConfig = {
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
   //   repoUrl: 'https://github.com/facebook/test-site',
+  customDocsPath: 'website/docs'
 };
 
 module.exports = siteConfig;
